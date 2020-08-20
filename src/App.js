d6154218cf404354e43953aedeb16d81bd7b1c1e
Patrick Wilker Vieira Mota <patrickvieiramota@gmail.com>
2020-08-20 14:41:50 -0300

    Create class components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import './App.css'
 
-import sky from './assets/sky.jpg'
+import TechList from './components/TechList'
 
 function App(){
-    return <img width="200px" src={sky} />
+    return <TechList />
 }
 
 export default App
